e5057913def0ee9b56f1ab9ff678d048a38121a2
johnlim2019 <69998939+johnlim2019@users.noreply.github.com>
2022-11-12 21:27:09 +0800

    Update: Added PlayerRound, Deck and Round classes Changes to be committed: 	new file:   .vscode/settings.json 	modified:   __pycache__/blackjack.cpython-310.pyc 	new file:   action.py 	modified:   blackjack.py 	modified:   tests.py

--- blackjack.py
+++ blackjack.py
@@ -1,89 +1,249 @@
 import pandas as pd
 import numpy as np
 import random
+from typing import List, Dict
 
 
-def getDeck(num):
-    faces = ['a', 'j', 'k', 'q']
-    for i in range(2, 11):
-        faces.append(str(i))
-    suits = ['spades', 'diamonds', 'clubs', 'hearts']
+class Deck:
+    def __init__(self, num, bankRoll):
+        faces = ["a", "j", "k", "q"]
+        for i in range(2, 11):
+            faces.append(str(i))
+        suits = ["spades", "diamonds", "clubs", "hearts"]
 
-    deck = []
-    for i in range(num):
-        for suit in suits:
-            for face in faces:
-                card = [face, suit]
-                deck.append(card)
-    return random.shuffle(deck)
+        deck = []
+        for i in range(num):
+            for suit in suits:
+                for face in faces:
+                    card = [face, suit]
+                    deck.append(card)
+        random.shuffle(deck)
+        self.deckCards = deck
+        self.roundCards = []
+        self.bankRoll = bankRoll
+        self.rawCount = 0
+        self.cardsDealt = 0
+
+    def drawCard(self):
+        card = self.deckCards.pop()
+        self.roundCards.append(card)
+        return card
+
+    def count(self, cards):
+        currentCount = 0
+        for card in cards:
+            if card[0] in ["10", "j", "k", "q", "a"]:
+                currentCount -= 1
+            elif card[0] in ["2", "3", "4", "5", "6"]:
+                currentCount += 1
+            else:
+                currentCount += 0
+        self.rawCount += currentCount
+        self.cardsDealt += len(cards)
+        self.roundCards = []
+
+    def getTrueCount(self):
+        if self.cardsDealt == 0:
+            return 0
+        else:
+            return round(self.rawCount / self.cardsDealt)
+
+    def endRound(self):
+        self.roundCards = []
 
 
-def getValue(hand):
-    value = 0
-    hasAce = 0
-    handType = 'hard'
-    for i in hand:
-        face = i[0]
+class PlayerRound:
+    def __init__(self, isDealer: bool):
+        self.isDealer = isDealer
+        self.hand = []
+        self.handType = "hard"
+        self.value = 0
+        self.betAmt = 0
+
+    def toString(self):
+        string = "is dealer: " + str(self.isDealer) + "\n"
+        string += "hand type: " + self.handType + "\n"
+        string += "value: " + str(self.value) + "\n"
+        string += "hand: " + str(self.hand) + "\n"
+        string += "bet: " + str(self.betAmt) + "\n"
+        print(string)
+        return string
+
+    def bet(self, value: int):
+        self.betAmt = self.betAmt + value
+
+    def addCardHand(self, card):
+        self.hand.append(card)
+
+    def getValue(self):
+        value = 0
+        hasAce = 0
+        handType = "hard"
+        for i in self.hand:
+            face = i[0]
+            if face == "j" or face == "k" or face == "q":
+                value += 10
+            elif face == "a":
+                value += 11
+                hasAce += 1
+                handType = "soft"
+            else:
+                value += int(face)
+        if hasAce > 0 and value > 21:
+            for i in range(hasAce):
+                value -= 10
+            handType = "hard"
+        if self.hand[0][0] == self.hand[1][0] and len(self.hand) == 2:
+            handType = "pair"
         # print(value)
-        if face == 'j' or face == 'k' or face == 'q':
-            value += 10
-        elif face == 'a':
-            value += 11
-            hasAce += 1
-            handType = 'soft'
+        # print(handType)
+        self.value = value
+        self.handType = handType
+        return value, handType
+
+    def decision(self, dealerCard):
+        dealerFace = dealerCard[0]
+        if self.handType == "hard":
+            if self.value >= 17:
+                return "s"
+            elif 12 < self.value < 17 and dealerFace in ["2", "3", "4", "5", "6"]:
+                return "s"
+            elif self.value == 12 and dealerFace in ["4", "5", "6"]:
+                return "s"
+            elif self.value == 11:
+                return "d"
+            elif self.value == 10 and dealerFace in ["10", "j", "q", "k", "a"]:
+                return "d"
+            elif self.value == 9 and dealerFace in ["3", "4", "5", "6"]:
+                return "d"
+            else:
+                return "h"
+        elif self.handType == "soft":
+            if self.value > 20:
+                return "s"
+            elif self.value == 19:
+                return "s"
+            elif self.value == 18 and dealerFace in ["2", "3", "4", "5", "6", "7", "8"]:
+                return "s"
+            elif self.value == 17 and dealerFace in ["3", "4", "5", "6"]:
+                return "d"
+            elif self.value == 16 and dealerFace in ["3", "4", "5"]:
+                return "d"
+            elif self.value == 15 and dealerFace in ["3", "4", "5"]:
+                return "d"
+            elif (self.value == 14 or self.value == 13) and dealerFace in ["5", "6"]:
+                return "d"
+            else:
+                return "h"
         else:
-            value += int(face)
-    if hasAce > 0 and value > 21:
-        for i in range(hasAce):
-            value -= 10
-        handType = 'hard'
-    if (hand[0][0] == hand[1][0]):
-        handType = 'pair'
-    # print(value)
-    # print(handType)
-    return value, handType
+            if self.hand[0][0] == "a":
+                return "split"
+            elif self.hand[0][0] in ["10", "k", "q", "j"]:
+                print("s")
+                return "s"
+            elif self.hand[0][0] == "9":
+                if dealerCard[0] in ["2", "3", "4", "5", "6", "8", "9"]:
+                    return "split"
+                else:
+                    return "s"
+            elif self.hand[0][0] == "8":
+                return "split"
+            elif self.hand[0][0] == "7":
+                if dealerCard[0] in ["2", "3", "4", "5", "6", "7"]:
+                    return "split"
+                else:
+                    "h"
+            elif self.hand[0][0] == "6":
+                if dealerCard[0] in ["2", "3", "4", "5", "6"]:
+                    return "split"
+                else:
+                    "h"
+            elif self.hand[0][0] == "5":
+                if dealerCard[0] in ["10", "a", "j", "q", "k"]:
+                    return "h"
+                else:
+                    return "d"
+            elif self.hand[0][0] == "4":
+                if dealerCard[0] in ["5", "6"]:
+                    return "split"
+                else:
+                    return "h"
+            elif self.hand[0][0] == "3" or self.hand[0][0] == "2":
+                if dealerCard[0] in ["2", "3", "4", "5", "6", "7"]:
+                    return "split"
+                else:
+                    return "h"
 
-
-def decision(dealerCard, value, type):
-    dealerFace = dealerCard[0]
-    if (type is True):
-        if (value >= 17):
-            return 's'
-        elif (12 < value < 17 and dealerFace in ['2', '3', '4', '5', '6']):
-            return 's'
-        elif (value == 12 and dealerFace in ['4', '5', '6']):
-            return 's'
-        elif (value == 11):
-            return 'd'
-        elif (value == 10 and dealerFace in ['10', 'j', 'q', 'k', 'a']):
-            return 'd'
-        elif (value == 9 and dealerFace in ['3', '4', '5', '6']):
-            return 'd'
+    def dealerDecision(self):
+        value, type = self.getValue()
+        if value < 17:
+            return "h"
         else:
-            return 'h'
-    else:
-        if (value > 20):
-            return 's'
-        elif (value == 19):
-            return 's'
-        elif (value == 18 and dealerFace in ['2', '3', '4', '5', '6', '7', '8']):
-            return 's'
-        elif (value == 17 and dealerFace in ['3', '4', '5', '6']):
-            return 'd'
-        elif (value == 16 and dealerFace in ['3', '4', '5']):
-            return 'd'
-        elif (value == 15 and dealerFace in ['3', '4', '5']):
-            return 'd'
-        elif ((value == 14 or value == 13) and dealerFace in ['5', '6']):
-            return 'd'
-        else:
-            return 'h'
+            return "s"
+
+    def clearHand(self):
+        self.hand = []
+        self.handType = "hard"
+        self.value = 0
+        self.betAmt = 0
 
 
-def count():
+class Round:
+    def __init__(self, deck: Deck, numPlayers: int, betSpread):
+        self.dealer = PlayerRound(True)
+        self.players: List[PlayerRound] = []
+        self.dealerCard = None
+        self.deck = deck
+        self.betSpread = betSpread
+        for i in range(numPlayers):
+            self.players.append(PlayerRound(False))
+
+    def bet(self):
+        if self.deck.getTrueCount() <= 2:
+            betValue = self.betSpread[2]
+        elif self.deck.getTrueCount() == 3:
+            betValue = self.betSpread[3]
+        elif self.deck.getTrueCount() == 4:
+            betValue = self.betSpread[4]
+        elif self.deck.getTrueCount() >= 5:
+            betValue = self.betSpread[5]
+        for player in self.players:
+            self.deck.bankRoll -= betValue
+            player.bet(betValue)
+
+    def deal(self):
+        for i in range(2):
+            card = self.deck.drawCard()
+            self.dealer.addCardHand(card)
+        for i in range(len(self.players)):
+            for j in range(2):
+                card = self.deck.drawCard()
+                self.players[i].addCardHand(card)
+        self.dealerCard = self.dealer.hand[0]
+        self.dealer.getValue()
+        for i in range(len(self.players)):
+            self.players[i].getValue()
+
+    def playIndex(self, num):
+        try:
+            assert self.dealerCard != None
+        except Exception as e:
+            print('dealer card is empty!')
+            raise(e)
+        playerHand = self.players[num]
+        decision = playerHand.decision(self.dealerCard)
+        if decision == 'h':
+            card = self.deck.drawCard()
+            playerHand.addCardHand(card)
     
-    return
+        elif decision == 'd':
+            playerHand.bet(playerHand.betAmt)
 
-def bet():
-    betSpread = [10,20,40,100]
-    return
+        elif decision == 's':
+            return playerHand
+        elif decision == 'split':
+            self.players.pop(num)
+            split1 = pop
+        return playerHand
+    
+    def play(self):
--- tests.py
+++ tests.py
@@ -1,32 +1,125 @@
 import blackjack
 
-handDouble = [
-    ['a','spades'],
-    ['a','hearts']
-]
+hands = []
+dealers = []
+corrects = []
 
-handSoft = [
-    ['a','spades'],
-    ['3','hearts']
-]
+hands.append([["5", "hearts"], ["10", "hearts"]])
+dealers.append(["j", "hearts"])
+corrects.append("h")
 
-handHard = [
-    ['5','spades'],
-    ['8','hearts']
-]
+hands.append([["q", "hearts"], ["a", "hearts"]])
+dealers.append(["8", "hearts"])
+corrects.append("s")
+
+hands.append([["q", "hearts"], ["a", "hearts"]])
+dealers.append(["8", "hearts"])
+corrects.append("s")
+
+hands.append([["j", "hearts"], ["a", "hearts"]])
+dealers.append(["4", "hearts"])
+corrects.append("s")
+
+hands.append([["k", "hearts"], ["k", "hearts"]])
+dealers.append(["8", "hearts"])
+corrects.append("s")
+
+hands.append([["j", "hearts"], ["a", "hearts"]])
+dealers.append(["4", "hearts"])
+corrects.append("s")
+
+hands.append([["3", "hearts"], ["2", "hearts"]])
+dealers.append(["a", "hearts"])
+corrects.append("h")
+
+hands.append([["7", "hearts"], ["8", "hearts"]])
+dealers.append(["3", "hearts"])
+corrects.append("s")
+
+hands.append([["6", "hearts"], ["q", "hearts"]])
+dealers.append(["4", "hearts"])
+corrects.append("s")
+
+hands.append([["2", "hearts"], ["5", "hearts"]])
+dealers.append(["2", "hearts"])
+corrects.append("h")
+
+hands.append([["q", "hearts"], ["4", "hearts"]])
+dealers.append(["9", "hearts"])
+corrects.append("h")
+
+hands.append([["2", "hearts"], ["3", "hearts"]])
+dealers.append(["q", "hearts"])
+corrects.append("h")
+
+hands.append([["2", "hearts"], ["5", "hearts"]])
+dealers.append(["q", "hearts"])
+corrects.append("h")
+
+hands.append([["a", "hearts"], ["k", "hearts"]])
+dealers.append(["q", "hearts"])
+corrects.append("s")
+
+hands.append([["2", "hearts"], ["2", "hearts"]])
+dealers.append(["6", "hearts"])
+corrects.append("split")
+
+hands.append([["6", "hearts"], ["3", "hearts"]])
+dealers.append(["q", "hearts"])
+corrects.append("h")
+
+hands.append([["a", "hearts"], ["j", "hearts"]])
+dealers.append(["8", "hearts"])
+corrects.append("s")
+
+hands.append([["j", "hearts"], ["3", "hearts"]])
+dealers.append(["3", "hearts"])
+corrects.append("s")
+
+hands.append([["10", "hearts"], ["12", "hearts"]])
+dealers.append(["4", "hearts"])
+corrects.append("s")
+
+hands.append([["2", "hearts"], ["q", "hearts"]])
+dealers.append(["q", "hearts"])
+corrects.append("h")
+
+player = blackjack.PlayerRound(False)
+
+for i in range(len(hands)):
+    # print(player.hand)
+    for card in hands[i]:
+        # print(card)
+        player.addCardHand(card)
+    player.getValue()
+    try:
+        print()
+        print(i)
+        assert player.decision(dealers[i]) == corrects[i]
+    except Exception as e:
+        print('actual: '+player.decision(dealers[i]))
+        print('expected: '+corrects[i])
+        print(player.handType)
+        print(hands[i])
+        print(dealers[i])
+        print(player.value)
+        raise(e)
+    player.clearHand()
 
 
-# print(blackjack.getValue(handHard))
-assert blackjack.getValue(handHard) == (13,'hard')
-assert blackjack.getValue(handSoft) == (14,'soft')
-assert blackjack.getValue(handDouble) == (2,'pair')
+deck = blackjack.Deck(6,5000)
+betSpread ={
+    2:5,
+    3:10,
+    4:20,
+    5:50
+}
+round = blackjack.Round(deck,2,betSpread)
+round.bet()
+round.deal()
+for i in round.players:
+    print(i)
+    i.toString()
+print(round.dealer)
+round.dealer.toString()
 
-
-dealerFace = ['2','3','4','5','6','7','8','9','10','j','q','k','a']
-value = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
-type = ['hard','soft','pair']
-
-print(blackjack.decision('a',2,'hard'))
-
-
-blackjack.decision('2',)
