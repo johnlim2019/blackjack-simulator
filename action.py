bd5140e0652bcb87909489113c201b4f3d4e4ddf
johnlim2019 <69998939+johnlim2019@users.noreply.github.com>
2023-01-17 21:26:24 +0800

    Update: 100 plays of 100 rounds

--- action.py
+++ action.py
@@ -1,6 +1,8 @@
 import blackjack
 betSpread = {2: 5, 3: 10, 4: 20, 5: 50}
 
-table = blackjack.Table(1,6,1500,betSpread,0.3)
-table.playRounds(180)
-table.outputLog()
+
+for i in range(100):
+    table = blackjack.Table(1,6,1500,betSpread,0.6)
+    table.playRounds(100)
+    table.outputLog()
